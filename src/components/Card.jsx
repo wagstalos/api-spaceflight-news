e15155b4b6be1f0db96553ba372455560ade30fc
Wagner Paulo <wagstalos@hotmail.com>
2021-12-18 16:13:25 -0300

    bugfix: padding mobile

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,13 +4,13 @@ import "./card.css"
 
 function Card({ id, title, imageUrl, publishedAt, newsSite, summary, url }) {
   return (
-    <div className="row fl-card p-4">
+    <div className="row fl-card p-2 p-md-4">
       <div key={id} className="fl-card__inner col-12 col-md-10 mx-auto d-flex flex-column flex-sm-row">
-        <div className="p-4">
+        <div className="p-2 p-md-4 mt-2 mt-md-0">
           <img className="img-fluid" src={imageUrl} alt={title} />
         </div>
 
-        <div className="p-4">
+        <div className="p-2 p-md-4">
           <h3> {title}</h3>
           <div className="d-flex justify-content-between mt-2 mb-4">
             <span>{publishedAt.slice(0, -14).split('-').reverse().join('/')}</span>
